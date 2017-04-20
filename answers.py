9e579cfd8a4fda48824f89d3c6fb0f792c3c9151
Brighticorn Hamilton <brighticorn@gmail.com>
2017-04-19 17:22:31 -0700

    adds the answer, truth

---
+++ answers.py
@@ -0,0 +1,12 @@
+# Answer to the Ultimate Question of Life, The Universe, and Everything
+question = "Answer to the Ultimate Question of Life, The Universe, \
+and Everything:"
+answer = 42
+
+reference = "https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=the+answer+to+life+the+universe+and+everything"
+
+print question
+print answer
+print 
+print "Reference"
+print reference
